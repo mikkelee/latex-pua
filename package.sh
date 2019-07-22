adea05f3b87e5c49c8d84017b335a0b9dc43df7d
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2019-07-22 19:37:54 +0200

    Packaging script hardening

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 cd ..
 zip -r unicode-alphabets/unicode-alphabets-TMP.zip\
-	unicode-alphabets/latex\
+	unicode-alphabets/latex/*.{sty,csv,tex}\
 	unicode-alphabets/docs/*.{tex,pdf,bib,txt}\
 	unicode-alphabets/README.md
 cd -
