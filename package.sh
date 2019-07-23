d4f44b7b8d24cdf206a7773f29e49bcffaaa5a66
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2019-07-23 09:33:34 +0200

    Fixed CTAN version
    Packaging script auto-date

--- package.sh
+++ package.sh
@@ -1,5 +1,7 @@
+perl -i -p -e"s#[12][0-9]{3}/[01][0-9]/[0-3][0-9]#$(date '+%Y/%m/%d')#" latex/unicode-alphabets.sty
+perl -i -p -e"s#[12][0-9]{3}-[01][0-9]-[0-3][0-9]#$(date '+%Y-%m-%d')#" README.md
 cd ..
-zip -r unicode-alphabets/unicode-alphabets-TMP.zip\
+zip -r unicode-alphabets/unicode-alphabets-$(date '+%Y%m%d').zip\
 	unicode-alphabets/latex/*.{sty,csv,tex}\
 	unicode-alphabets/docs/*.{tex,pdf,bib,txt}\
 	unicode-alphabets/README.md
